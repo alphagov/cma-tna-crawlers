17d1644cfa4cd474cae5ab3ac084869c380d55bb
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-12-08 14:09:16 +0000

    Parse single CC case list page
    * Has links to other letter pages if on A
    * Has links to cases
    * Nothing much else yet

---
+++ lib/cma/cc/case_list/page.rb
@@ -0,0 +1,34 @@
+require 'nokogiri'
+
+module CMA
+  module CC
+    module CaseList
+      class Page
+        class Link < Struct.new(:href, :title)
+        end
+
+        attr_accessor :doc
+        def initialize(doc)
+          self.doc = doc
+        end
+
+        def first_page?
+          @first_page ||= (doc.css('.atoz li strong').text == 'A')
+        end
+
+        def old_case_links
+          @old_case_links ||= doc.css('#listing .itemDetails a').map {|a| Link.new(a['href'], a.text) }
+        end
+
+        def letter_page_links
+          return [] unless first_page?
+          @letter_page_links ||= doc.css('.atoz a').map {|a| Link.new(a['href'], a.text)}
+        end
+
+        def self.from_html(doc)
+          Page.new(doc)
+        end
+      end
+    end
+  end
+end
---
+++ spec/cma/cc/case_list/page_spec.rb
@@ -0,0 +1,68 @@
+require 'spec_helper'
+require 'cma/cc/case_list/page'
+
+module CMA::CC::CaseList
+  describe Page do
+    describe 'from_html' do
+      subject(:page) { Page.from_html(Nokogiri::HTML(File.read("spec/fixtures/cc/#{filename}"))) }
+
+      context 'we are on the page for the letter A' do
+        let(:filename) { 'our-work-directory-a.html' }
+
+        it { should be_first_page }
+
+        it 'has links to all the old cases' do
+          expect(page.old_case_links.size).to eq(13)
+        end
+
+        describe 'the first old case link' do
+          subject(:link) { page.old_case_links.first }
+
+          it 'has a title' do
+            expect(link.title).to eq(
+              'AAH Pharmaceuticals Limited / East Anglian Pharmaceuticals Limited merger inquiry')
+          end
+
+          it 'has a TNA link' do
+            expect(link.href).to eq(
+              'http://webarchive.nationalarchives.gov.uk/20140402141250/'\
+              'http://www.competition-commission.org.uk/our-work/directory-of-all-inquiries/aah-pharmaceuticals-limited-east-anglian-pharmaceuticals-limited')
+          end
+        end
+
+        it 'has links to all the other letter pages (some are missing)' do
+          expect(page.letter_page_links.size).to eq(22)
+        end
+
+        describe 'the first letter link' do
+          subject(:link) { page.letter_page_links.first }
+
+          it 'has a letter for a title' do
+            link.title.should == 'B'
+          end
+
+          it 'links to the B page' do
+            expect(link.href).to eq(
+              'http://webarchive.nationalarchives.gov.uk/20140402141250/'\
+              'http://www.competition-commission.org.uk/our-work/directory-of-all-inquiries?bytype=atoz&byid=b')
+          end
+        end
+
+      end
+
+      context 'we are on the page for the letter B' do
+        let(:filename) { 'our-work-directory-b.html' }
+
+        it { should_not be_first_page }
+
+        it 'has links to all the old cases' do
+          expect(page.old_case_links.size).to eq(14)
+        end
+
+        it 'does not link to all the other letter pages' do
+          expect(page.letter_page_links.size).to eq(0)
+        end
+      end
+    end
+  end
+end
