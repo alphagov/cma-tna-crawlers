d59bf891ed8d40b6cd87b543d21e638b72e3916a
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-12-09 10:48:53 +0000

    CC::Case#add_case_detail
    * Parse dates from markup

--- lib/cma/cc/case.rb
+++ lib/cma/cc/case.rb
@@ -1,7 +1,7 @@
 module CMA
   module CC
     class Case
-      attr_accessor :original_url, :title
+      attr_accessor :original_url, :title, :date_of_referral, :statutory_deadline
       def initialize(original_url, title)
         self.original_url  = original_url
         self.title = title
@@ -13,6 +13,31 @@ module CMA
 
       def self.from_link(link)
         Case.new(link.original_url, link.title)
+      end
+
+      def add_case_detail(doc)
+        self.date_of_referral   = parse_date_at_xpath(
+          doc, [possible_date_position_1(2), possible_date_position_2(2)])
+        self.statutory_deadline = parse_date_at_xpath(
+          doc, [possible_date_position_1(3), possible_date_position_2(3)])
+      end
+
+    private
+      # Dates could be here
+      def possible_date_position_1(index)
+        "//div[@id='mainColumn']/h1/following-sibling::p/text()[#{index}]"
+      end
+
+      # Or here, depending on value of $WTFICANTEVEN
+      def possible_date_position_2(index)
+        "//div[@id='mainColumn']/h1/following-sibling::div/p[1]/text()[#{index}]"
+      end
+
+      def parse_date_at_xpath(doc, try_xpath)
+        xpath = try_xpath.find { |xpath| doc.at_xpath(xpath) }
+        return if (date_node = doc.at_xpath(xpath)).nil?
+        date_node.text =~ /([0-9]{2}\.[0-9]{2}\.[0-9]{2})/
+        Date.strptime($1, '%d.%m.%y') if xpath
       end
     end
   end
--- spec/cma/cc/case_spec.rb
+++ spec/cma/cc/case_spec.rb
@@ -3,15 +3,15 @@ require 'cma/cc/case'
 
 module CMA::CC
   describe Case do
-    describe '.from_link' do
-      let(:link)  { double('link', href: href, title: title, original_url: original_url) }
-      let(:href)  { 'Should not be used' }
-      let(:title) { 'A title' }
-      let(:original_url) do
-        'http://www.competition-commission.org.uk/our-work/directory-of-all-inquiries/'\
+    let(:link)  { double('link', href: href, title: title, original_url: original_url) }
+    let(:href)  { 'Should not be used' }
+    let(:title) { 'A title' }
+    let(:original_url) do
+      'http://www.competition-commission.org.uk/our-work/directory-of-all-inquiries/'\
         'alpha-flight-group-limited-lsg-lufthansa-service-holding-ag-merger-inquiry'
-      end
+    end
 
+    describe '.from_link' do
       before { expect(link).not_to receive(:href) }
 
       subject(:_case) { Case.from_link(link) }
@@ -32,6 +32,21 @@ module CMA::CC
         it 'uses the original_url from the link' do
           expect(_case.original_url).to eql(original_url)
         end
+      end
+    end
+
+    describe '#add_case_detail' do
+      let(:doc) { Nokogiri::HTML(File.read('spec/fixtures/cc/archived-alpha-flight.html')) }
+
+      subject(:_case) { Case.new(original_url, title) }
+
+      before { _case.add_case_detail(doc) }
+
+      it 'parses the date of referral' do
+        expect(_case.date_of_referral).to eql(Date.new(2011, 10, 10))
+      end
+      it 'parses the statutory deadline' do
+        expect(_case.statutory_deadline).to eql(Date.new(2012, 3, 25))
       end
     end
   end
