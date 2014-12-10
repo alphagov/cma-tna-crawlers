70c7afcaffbf4baee75d535b373264ade55b413b
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-12-10 11:35:32 +0000

    Remove unused letter_page_links from CMA::Case
    * Crawler's doing that work from link soup/regex anyway

--- lib/cma/cc/case_list/page.rb
+++ lib/cma/cc/case_list/page.rb
@@ -9,17 +9,8 @@ module CMA
           self.doc = doc
         end
 
-        def first_page?
-          @first_page ||= (doc.css('.atoz li strong').text == 'A')
-        end
-
         def old_case_links
           @old_case_links ||= doc.css('#listing .itemDetails a').map {|a| Link.new(a['href'], a.text) }
-        end
-
-        def letter_page_links
-          return [] unless first_page?
-          @letter_page_links ||= doc.css('.atoz a').map {|a| Link.new(a['href'], a.text)}
         end
 
         def self.from_html(doc)
--- spec/cma/cc/case_list/page_spec.rb
+++ spec/cma/cc/case_list/page_spec.rb
@@ -9,8 +9,6 @@ module CMA::CC::CaseList
       context 'we are on the page for the letter A' do
         let(:filename) { 'our-work-directory-a.html' }
 
-        it { should be_first_page }
-
         it 'has links to all the old cases' do
           expect(page.old_case_links.size).to eq(13)
         end
@@ -28,39 +26,6 @@ module CMA::CC::CaseList
               'http://webarchive.nationalarchives.gov.uk/20140402141250/'\
               'http://www.competition-commission.org.uk/our-work/directory-of-all-inquiries/aah-pharmaceuticals-limited-east-anglian-pharmaceuticals-limited')
           end
-        end
-
-        it 'has links to all the other letter pages (some are missing)' do
-          expect(page.letter_page_links.size).to eq(22)
-        end
-
-        describe 'the first letter link' do
-          subject(:link) { page.letter_page_links.first }
-
-          it 'has a letter for a title' do
-            expect(link.title).to eql('B')
-          end
-
-          it 'links to the B page' do
-            expect(link.href).to eq(
-              'http://webarchive.nationalarchives.gov.uk/20140402141250/'\
-              'http://www.competition-commission.org.uk/our-work/directory-of-all-inquiries?bytype=atoz&byid=b')
-          end
-        end
-
-      end
-
-      context 'we are on the page for the letter B' do
-        let(:filename) { 'our-work-directory-b.html' }
-
-        it { should_not be_first_page }
-
-        it 'has links to all the old cases' do
-          expect(page.old_case_links.size).to eq(14)
-        end
-
-        it 'does not link to all the other letter pages' do
-          expect(page.letter_page_links.size).to eq(0)
         end
       end
     end
