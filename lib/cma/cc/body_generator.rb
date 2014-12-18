69193b5ee4885ab12310e6510e9f838b5ca62834
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-12-18 18:12:50 +0000

    Port body generator
    * Allow generation of body in order from CC markup_sections
    * Remove OFT code - this leaves one weird `## Phase 2` for now

---
+++ lib/cma/cc/body_generator.rb
@@ -0,0 +1,98 @@
+require 'cma/case_store'
+require 'kramdown'
+
+module CMA
+  module CC
+    class BodyGenerator
+      attr_accessor :_case
+
+      def initialize(_case)
+        self._case = _case
+      end
+
+      def generate!
+        _case.body = "## Phase 2\n\n"
+
+        _case.body << "Date of referral:  #{reformat_date(_case.date_of_referral)}\n"
+        _case.body <<
+          "Statutory deadline:  #{reformat_date(_case.statutory_deadline)}\n" if _case.statutory_deadline
+        _case.body << "\n"
+
+        append_single_sections(
+          %w(
+            core_documents
+            remittal
+            undertakings-and-order
+            final_report
+            provisional-final-report
+            annotated-issues-statement
+          )
+        )
+
+        append_subsections(
+          %w(
+            evidence
+            analysis
+          )
+        )
+
+        append_single_sections(
+          %w(
+            news-releases
+            news-releases-announcements
+          )
+        )
+
+        append_oft_sections
+      end
+
+      def append_subsections(subsection_names)
+        subsection_names.each do |prefix|
+          in_order_subsections = _case.markup_sections.keys.select do |section_name|
+            section_name =~ Regexp.new("^#{prefix}/")
+          end
+
+          in_order_subsections.each do |section_name|
+            _case.markup_sections[section_name].tap do |content|
+              _case.body << Kramdown::Document.new(content, header_offset: 1).to_kramdown
+            end
+          end
+        end
+      end
+
+      def append_bodies(bodies, options = { header_offset: 1 })
+        bodies.each do |section_body|
+          Kramdown::Document.new(
+            section_body, header_offset: options[:header_offset]
+          ).tap do |tree|
+            _case.body << tree.to_kramdown
+          end
+        end
+      end
+
+      def append_single_sections(section_names)
+        bodies = section_names.map { |cc_section_name| _case.markup_sections[cc_section_name] }.compact
+
+        append_bodies(bodies)
+      end
+
+
+      def reformat_date(value)
+        Date.strptime(value, '%Y-%m-%d').strftime('%d/%m/%Y')
+      end
+
+      def append_oft_sections
+        bodies = _case.markup_sections.inject([]) do |bodies, name_content|
+          bodies << name_content.last if name_content.first =~ %r{OFT/}
+          bodies
+        end
+
+        if bodies.any?
+          _case.body << "\n## Phase 1\n"
+
+          append_bodies(_case, bodies, header_offset: 2)
+        end
+      end
+    end
+  end
+end
---
+++ spec/cma/cc/body_generator_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+require 'cma/cc/body_generator'
+require 'cma/case_store'
+
+describe CMA::CC::BodyGenerator do
+
+  describe 'creating a new one' do
+    let(:location)      { 'spec/fixtures/cc' }
+    let(:case_store)    { CMA::CaseStore.new(location) }
+    let(:case_filename) { 'our-work-directory-of-all-inquiries-aeg-wembley-arena.json' }
+    let(:_case)         { case_store.find('http://example.com/our-work/directory-of-all-inquiries/aeg-wembley-arena') }
+
+    subject(:body_generator) { CMA::CC::BodyGenerator.new(_case) }
+
+    describe '#generate!' do
+      before do
+        body_generator.generate!
+      end
+
+      subject(:case_body) { _case.body }
+
+      it 'begins with an h2 Phase 2 header' do
+        expect(case_body).to match /^## Phase 2/
+      end
+
+      it 'uses the date of referral' do
+        expect(case_body).to have_content('Date of referral:  22/03/2013').under('## Phase 2')
+      end
+      it 'uses the statutory deadline' do
+        expect(case_body).to have_content('Statutory deadline:  05/09/2013').under('## Phase 2')
+      end
+
+      it 'puts the CC headers at h3' do
+        expect(case_body).to include('### Core documents')
+      end
+      it 'excludes the useless analysis page' do
+        expect(case_body).to_not include("Analysis\n\n* [Working\n  papers]")
+      end
+
+      describe 'included useful evidence content' do
+        it { should include '### Summaries of hearings held with third parties' }
+        it { should include '### Initial submissions' }
+      end
+
+      describe 'the news section' do
+        it { should include '### News releases' }
+        it { should include '[CC to investigate AEG/Wembley Arena' }
+      end
+    end
+  end
+
+end
