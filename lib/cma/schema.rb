982c836267336941ac2dd4de1d057428a41800c0
Russell Garner <rgarner@zephyros-systems.co.uk>
2015-01-12 14:04:31 +0000

    Add a CMA::Schema that can:
    * Look up allowed values for market_sector and outcome_type from the schema JSON copied from
      https://github.com/alphagov/finder-api/blob/master/schemas/cma-cases.json

---
+++ lib/cma/schema.rb
@@ -0,0 +1,28 @@
+require 'json'
+
+module CMA
+  class Schema
+    def outcome_types
+      @_outcome_types ||= get_allowed_value_hash('outcome_type')
+    end
+
+    def market_sector
+      @_market_sector ||= get_allowed_value_hash('market_sector')
+    end
+
+    def get_allowed_value_hash(key)
+      keyed_section = json_hash['facets'].find { |hash| hash['key'] == key }
+
+      keyed_section['allowed_values'].inject({}) do |output_hash, label_value|
+        label, value = label_value['label'], label_value['value']
+
+        output_hash[label] = value
+        output_hash
+      end
+    end
+  private
+    def json_hash
+      @_json_hash ||= JSON.parse(File.read('schema/cma-cases.json'))
+    end
+  end
+end
---
+++ spec/cma/schema_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+require 'cma/schema'
+
+describe CMA::Schema do
+  let(:schema) { CMA::Schema.new }
+  describe '#outcome_types' do
+    subject { schema.outcome_types }
+
+    it { should be_a(Hash) }
+    it 'resolves titles to values' do
+      expect(
+        schema.outcome_types['Markets - phase 1 referral']
+      ).to eql('markets-phase-1-referral')
+    end
+  end
+
+  describe '#market_sector' do
+    subject { schema.market_sector }
+
+    it { should be_a(Hash) }
+    it 'resolves titles to values' do
+      expect(
+        schema.market_sector['Healthcare and medical equipment']
+      ).to eql('healthcare-and-medical-equipment')
+    end
+  end
+end
