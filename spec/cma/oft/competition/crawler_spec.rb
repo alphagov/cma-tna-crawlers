0943d00d813a7b0a62d2818738a872f127a413c3
Michael Patrick <michael.patrick@digital.cabinet-office.gov.uk>
2015-04-14 13:55:47 +0100

    Test that sidebar link nodes are being scraped
    These specs are currently failing, until we make our changes. Unfortunately it
    seems there isn't much test coverage in the project already of the parts we're
    making changes to, so we need to start with something to reassure us.
    
    Ideally these specs would use test doubles rather than this terrible approach,
    but unless anybody suggests a nice way to do that in this particular situation,
    we're a bit stuck picturing a mess of nested doubles, stubbing left, right and
    centre.

--- spec/cma/oft/competition/crawler_spec.rb
+++ spec/cma/oft/competition/crawler_spec.rb
@@ -8,4 +8,38 @@ module CMA::OFT::Competition
         'http://oft.gov.uk/OFTwork/competition-act-and-cartels/ca98/decisions/reckitt-benckiser')
     end
   end
+
+  describe '#link_nodes_for' do
+    let(:crawler) { Crawler.new }
+    let(:main_page_link_nodes) { 3 }
+    let(:sidebar_link_nodes) { 2 }
+
+    context 'with a non-CASE_DETAIL page' do
+      let(:page) {
+        Anemone::Page.new(
+          'http://www.oft.gov.uk/OFTwork/competition-act-and-cartels/ca98/',
+          :body => File.read('spec/fixtures/oft/decisions-aberdeen-journals2.html'),
+          :headers => {'content-type' => ['text/html']},
+        )
+      }
+
+      it 'should scrape main page link nodes only' do
+        expect(crawler.link_nodes_for(page).size).to eq(main_page_link_nodes)
+      end
+    end
+
+    context 'with a CASE_DETAIL page' do
+      let(:page) {
+        Anemone::Page.new(
+          'http://www.oft.gov.uk/OFTwork/competition-act-and-cartels/ca98/decisions/aberdeen-journals2',
+          :body => File.read('spec/fixtures/oft/decisions-aberdeen-journals2.html'),
+          :headers => {'content-type' => ['text/html']},
+        )
+      }
+
+      it 'should scrape both main page and sidebar link nodes' do
+        expect(crawler.link_nodes_for(page).size).to eq(main_page_link_nodes + sidebar_link_nodes)
+      end
+    end
+  end
 end
